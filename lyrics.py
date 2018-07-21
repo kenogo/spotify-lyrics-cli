4ef94e3231b22a67d6ce63be6c4e8a86456d6c9f
Keno Goertz <keno@goertz-berlin.com>
2018-07-21 20:50:07 +0200

    Complete clean rewrite, switched to pure python

--- lyrics.py
+++ lyrics.py
@@ -1,8 +1,8 @@
-import requests
-import sys
+import argparse
 from bs4 import BeautifulSoup
-import os
+import dbus
 import re
+import requests
 
 try:
     from urllib.parse import quote_plus
@@ -10,82 +10,66 @@ except ImportError:
     from urllib import quote_plus
 
 
-def get_lyrics(song_name):
-    song_name = re.sub(r'[\(\[].*?[\)\]]', '', song_name)
-    search_name = song_name + ' genius lyrics'
+def get_lyrics_genius(artist, title):
+    # Google for Lyrics
+    search_name = "%s %s genius lyrics" % (artist, title)
     name = quote_plus(search_name)
-    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11'
-           '(KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-           'Accept-Language': 'en-US,en;q=0.8',
-           'Connection': 'keep-alive'}
-
     url = 'http://www.google.com/search?q=' + name
-
-    result = requests.get(url, headers=hdr).text
+    result = requests.get(url).text
     link_start = result.find('https://genius.com')
-
     if(link_start == -1):
-        return get_lyrics_musixmatch(song_name)
-        
+        return "Lyrics could not be found..."
     link_end = result.find('"', link_start + 1)
-    link = result[link_start:link_end]
+    link = result[link_start:link_end].lower()
+    link = re.sub(r"&.*", '', link) # Remove PHP nonesense
+    link_correct = check_link_genius(artist, title, link)
+    if not link_correct:
+        return "Lyrics could not be found..."
 
-    check_link = link.lower()
-    song_name_arr = song_name.split()
-    song_name_arr = [s.lower() for s in song_name_arr]
-    # Replace special characters:
-    song_name_arr = [re.sub("[^a-z^A-Z^0-9]", ".*", s) +
-            ".*" for s in song_name_arr]
-    link_check = "https://genius.com/"
-    for s in song_name_arr:
-        link_check += s
-    if re.match(link_check, check_link) is None:
-        return get_lyrics_musixmatch(song_name)
-
-    lyrics_html = requests.get(link, headers={
-                               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel'
-                               'Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, '
-                               'like Gecko) Chrome/55.0.2883.95 Safari/537.36'
-                               }
-                               ).text
-
+    # Get the lyrics from genius
+    lyrics_html = requests.get(link).text
     soup = BeautifulSoup(lyrics_html, "lxml")
     raw_lyrics = str(soup.findAll('div', attrs={'class': 'lyrics'}))
     lyrics = raw_lyrics[1:len(raw_lyrics)-1]
-    lyrics = re.sub(r"<[^<>]*>", '', lyrics)
-    return lyrics[2:len(lyrics)]
-
-def get_lyrics_musixmatch(song_name):
-    search_name = song_name + ' musixmatch'
-    name = quote_plus(search_name)
-    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11'
-           '(KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-           'Accept-Language': 'en-US,en;q=0.8',
-           'Connection': 'keep-alive'}
-
-    url = 'http://www.google.com/search?q=' + name
-
-    result = requests.get(url, headers=hdr).text
-    link_start = result.find('https://www.musixmatch.com')
-
-    if(link_start == -1):
-        return("Lyrics not found on genius or musixmatch")
-        
-    link_end = result.find('"', link_start + 1)
-    link = result[link_start:link_end]
-
-    lyrics_html = requests.get(link, headers={
-                               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel'
-                               'Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, '
-                               'like Gecko) Chrome/55.0.2883.95 Safari/537.36'
-                               }
-                               ).text
-
-    soup = BeautifulSoup(lyrics_html, "lxml")
-    raw_lyrics = str(soup.findAll('p', attrs={'class': 'mxm-lyrics__content'}))
-    lyrics = raw_lyrics[1:len(raw_lyrics)-1]
-    lyrics = re.sub(r"<[^<>]*>", '', lyrics)
+    lyrics = re.sub(r"<[^<>]*>", '', lyrics) # Remove HTML tags
+    lyrics = lyrics[2:] # Remove two newlines
     return lyrics
 
-print(sys.argv[2].upper()+' - '+sys.argv[1].upper()+'\n')
-print(get_lyrics(sys.argv[1]+' '+sys.argv[2]))
+def check_link_genius(artist, title, link):
+    songinfo = "%s %s" % (artist, title)
+    songinfo = songinfo.lower()
+    songinfo = re.sub(r"[^a-zA-Z0-9 ]", '', songinfo) # Remove special chars
+    songinfo_array = songinfo.split()
+    for item in songinfo_array:
+        if link.find(item) == -1:
+            return False
+    return True
+
+def get_song_info(player):
+    bus = dbus.SessionBus()
+    try:
+        proxy = bus.get_object("org.mpris.MediaPlayer2.%s" % player,
+                               "/org/mpris/MediaPlayer2")
+    except dbus.exceptions.DBusException:
+        print("[ERROR] Player \"%s\" doesn't exist or isn't playing" % player)
+        return
+
+    interface = dbus.Interface(
+        proxy, dbus_interface="org.freedesktop.DBus.Properties"
+    )
+    properties = interface.GetAll("org.mpris.MediaPlayer2.Player")
+    metadata = properties["Metadata"]
+    artist = str(metadata["xesam:artist"][0])
+    title = str(metadata["xesam:title"])
+    return artist, title
+
+def main():
+    parser = argparse.ArgumentParser(description="Display lyrics automatically")
+    parser.add_argument("player", type=str, help="e.g. spotify,vlc,...")
+    args = parser.parse_args()
+    artist, title = get_song_info(args.player)
+    print("%s - %s\n" % (artist.upper(), title.upper()))
+    print(get_lyrics_genius(artist, title))
+
+if __name__ == "__main__":
+    main()
